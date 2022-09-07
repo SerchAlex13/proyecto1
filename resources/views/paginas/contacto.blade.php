7d3c85d7e2c09311aba6502597733240fba838e5
SerchAlex13 <sergio.rosales5262@alumnos.udg.mx>
2022-09-07 16:44:39 -0500

    Contacto

--- resources/views/paginas/contacto.blade.php
+++ resources/views/paginas/contacto.blade.php
@@ -1,13 +1,40 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Contacto</title>
-</head>
-<body>
-    <h1>Contacto</h1>
-    <a href="landingpage" class="nav-item nav-link active">Home</a>
-</body>
+<html lang="es">
+
+    <head>
+        <meta charset="utf-8">
+        <title>Contacto</title>
+    </head>
+
+    <body>
+        <h1>Contacto</h1>
+        <ul>
+            @foreach($versiones as $ver)
+                <li>{{ $ver }}</li>
+            @endforeach
+        </ul>
+
+        @if(!empty($version))
+            <p>
+                {{ $version }}
+            </p>
+        @endif
+
+        <form method="GET" action="contacto.blade.php">
+            <label for="name">
+                Nombre: <input type="text" id="name" name="nombre">
+            </label>
+            <br>
+            <label for="mail">
+                Correo: <input type="email" id="mail" name="correo">
+            </label>
+            <br>
+            <label for="comment">
+                Comentario: <br><textarea id="comment" name="comentario" cols="40" rows="5"></textarea>
+            </label>
+            <br>
+            <button type="submit">Enviar</button>
+        </form>
+    </body>
+
 </html>
--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,23 @@ Route::get('/landingpage', function () {
     return view('paginas.landingpage');
 });
 
-Route::get('/contacto', function () {
-    return view('paginas.contacto');
+Route::get('/contacto/{version_id?}', function ($version_id = null) {
+    
+    $versiones = [
+        'Datos v1',
+        'Datos v2',
+        'Datos v3',
+        'Datos v4',
+    ];
+
+    if (!empty($version_id)) {
+        $version = $versiones[$version_id];
+    } else {
+        $version = null;
+    }
+    
+    return view('paginas.contacto', compact('versiones', 'version'));
 });
+
+/*En la carpeta public sólo tener css, js, fonts, img
+para la entrega hacer una carpeta, por ejemplo proy1 y dentro meter lo que hay en public(css, js, fonts, img) y en index.html copiar lo de landingpage.blade.php*/
